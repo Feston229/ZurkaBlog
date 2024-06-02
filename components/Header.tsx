01fdfb3adcc291d1251ca56d9007b0d05c68a0f2
Feston229 <londonter@protonmail.com>
2024-06-02 15:17:55 +0700

    updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,15 +6,19 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
+/*
+21 - 22
+  <div className="mr-3">
+    <Logo />
+  </div>
+*/
+
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
